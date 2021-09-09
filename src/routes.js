cfbb356f3e144f39847584b896b5b7bb909d1675
Ricardo Augusto de Sousa Garcia da Silva <ricardoaugustogarcia@hotmail.com>
2021-09-09 08:49:19 -0300

    Configurando o projeto

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,6 @@ import { Router } from "express";
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ Message: "Hello world!!!" });
-});
+routes.get("/", (req, res) => res.json({ Message: "Hello world!!!" }));
 
 export default routes;
