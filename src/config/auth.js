7c5c7512d9e585c343f14a98496af2e4d478b82b
ricardoasgs <ricardoaugustogarcia@hotmail.com>
2021-09-13 08:35:11 -0300

    Session controller e autenticação JWT

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'df3ae1d225e7138a5bbdbf864ec8f584',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
+
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
@@ -8,5 +10,7 @@ routes.get('/', async (req, res) => {
 });
 
 routes.post('/users', UserController.store);
+
+routes.post('/sessions', SessionController.store);
 
 export default routes;
