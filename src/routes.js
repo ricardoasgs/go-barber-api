71e4abc9e2d98818ac886af26686c59d77d1707d
ricardoasgs <ricardoaugustogarcia@hotmail.com>
2021-09-13 19:57:28 -0300

    Upload de arquivos com multer, Model e controller de arquivos e relacionamento do avatar do usuário

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import AuthMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.get('/', async (req, res) => {
   res.json({ status: 'OK' });
@@ -18,5 +22,7 @@ routes.post('/sessions', SessionController.store);
 routes.use(AuthMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
