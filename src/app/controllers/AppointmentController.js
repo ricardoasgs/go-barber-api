81b889b1d256c91c942a080e44260caeb51aa8bc
ricardoasgs <ricardoaugustogarcia@hotmail.com>
2021-09-15 08:19:14 -0300

    validações de agendamento, listagem de agendamentos de usuário e paginação de agendamentos

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,10 +1,38 @@
 import * as Yup from 'yup';
+import { parseISO, startOfHour, isBefore } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
+import File from '../models/File';
 
 class AppointmentController {
   async index(req, res) {
-    return res.json('ok');
+    const { page = 1 } = req.query;
+
+    const appointments = await Appointment.findAll({
+      where: {
+        user_id: req.userId,
+        canceled_at: null,
+      },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+      ],
+    });
+    return res.json(appointments);
   }
 
   async store(req, res) {
@@ -29,6 +57,32 @@ class AppointmentController {
       return res
         .status(401)
         .json({ error: 'you can only create appointments with providers' });
+    }
+
+    const hourStart = startOfHour(parseISO(date));
+
+    /*
+    Check for Past Dates
+    */
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'past dates are not permitted' });
+    }
+
+    /*
+    Check for date availability
+    */
+    const checkAvalilability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvalilability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not availabe' });
     }
 
     const appointment = await Appointment.create({
