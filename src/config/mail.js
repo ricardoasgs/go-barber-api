bf691351a72637fd462ee8f6a5b6b84605190dee
ricardoasgs <ricardoaugustogarcia@hotmail.com>
2021-09-17 09:48:30 -0300

    Marcando notificações como lidas, cancelamento de agendamentos, configurando nodemailer e enviando email quando um agendamento é cancelado

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: '7ed4fc42f0775e',
+    pass: '62e9ca01810d43',
+  },
+  secure: false,
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
